ff58452d9d30e9d2002ca42841f6442fcf1d17be
markjun565 <markjun565@yahoo.com>
2014-04-29 13:26:22 +0800

    Create ftpserver.js

---
+++ ftpserver.js
@@ -0,0 +1,22 @@
+getServer(id)
+array (
+  'Server' => 
+  array (
+    'id' => '1',
+    'name' => 'Test Server',
+    'ip' => '',
+    'port' => '25565',
+    'dir' => 'server1',
+    'world' => '',
+    'players' => '8',
+    'memory' => '0',
+    'start_memory' => '0',
+    'jarfile' => '',
+    'autostart' => '0',
+    'default_level' => '10',
+    'daemon_id' => '1',
+    'announce_save' => '1',
+    'kick_delay' => '3000',
+    'suspended' => '0',
+  ),
+)
