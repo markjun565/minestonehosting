2e9c6ef577735ca294d78e922a3d8ad271cbe5df
markjun565 <markjun565@yahoo.com>
2014-04-29 14:30:51 +0800

    Create oneclick.js

---
+++ oneclick.js
@@ -0,0 +1,23 @@
+/* button#start.btn btn-success.onclick = */
+start();
+
+/* button.btn btn-warning.onclick = */
+backup();
+
+/* button.btn btn-danger.onclick = */
+$('#reinstall').modal();
+
+/* button#plugin-install.btn btn-primary install.onclick = */
+installversion('plugin');
+
+/* a.reload.onclick = */
+refreshlist('whitelist');
+
+/* a.reload.onclick = */
+refreshlist('op');
+
+/* button.btn btn-success.onclick = */
+reinstall();
+
+/* button.btn btn-success.onclick = */
+performbackup();
